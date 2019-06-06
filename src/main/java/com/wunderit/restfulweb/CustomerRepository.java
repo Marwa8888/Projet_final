2339e814834e06e110da8c10c459d62e0b7cdbfb
di-amine <di-amine@hotmail.fr>
2019-06-06 17:58:50 +0200

    role mogodb

--- src/main/java/com/wunderit/restfulweb/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wunderit.restfulweb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-
-    public Customer findByLastName(String lastName);
-
-}
